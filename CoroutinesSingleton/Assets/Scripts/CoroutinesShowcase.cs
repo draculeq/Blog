3574a3d4477bb9367152c62a4639b0421b828741
draculeq <gsniecinski@gmail.com>
2019-09-12 20:58:16 +0200

    CoroutinesSingleton

---
+++ CoroutinesSingleton/Assets/Scripts/CoroutinesShowcase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Deadbit.Utils;
+using Deadbit.Utils.Extensions;
+using UnityEngine;
+
+class CoroutinesShowcase : MonoBehaviour
+{
+    void Start()
+    {
+        this.StartDelayed(1.5f, () => Debug.Log("I am the message! (delayed by 1.5sec)"));
+        Coroutines.Instance.StartDelayed(3, () => Debug.Log("I am the message! (delayed by 3 frames)"));
+        Coroutines.Instance.Begin(TestCoroutine());
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        for (int i = 0; i<5; i++)
+{
+            yield return new WaitForSeconds(1);
+            Debug.Log("Test: " + i);
+        }
+    }
+}
---
+++ CoroutinesSingleton/Assets/Scripts/CoroutinesTest.cs
@@ -0,0 +1,22 @@
+﻿using Deadbit.Utils;
+using Deadbit.Utils.Extensions;
+using UnityEngine;
+
+class CoroutinesTest : MonoBehaviour
+{
+    void Start()
+    {
+        Log("Preparing messages...");
+        this.StartDelayed(1.5f, () => Log("I am the message! (delayed by 1.5sec)"));
+        this.StartDelayed(3, () => Log("I am the message! (delayed by 3 frames)"));
+        Log("Message sent!");
+
+
+        Coroutines.Instance.StartDelayed(1f, () => Log("Coroutines Test"));
+    }
+
+    private void Log(string message)
+    {
+        Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, this, string.Format("[Frame: {1}, Time: {2}] {0}", message, Time.frameCount, Time.time));
+    }
+}
